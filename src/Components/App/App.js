1adfa5416308e670b3ac6394c4506451f105e0f7
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-11 13:32:35 +0200

    implement app routing

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from '../Layout/Layout';
+import NotFound from '../NotFound/NotFound';
+import Home from '../Home/Home';
+import About from '../About/About';
+import Pricing from '../Pricing/Pricing';
+import Contact from '../Contact/Contact';
 
 
 function App() {
+  const routes=createBrowserRouter([
+    {path:'',element:<Layout/>,errorElement:<NotFound/>,  children:[
+      {index:true,element:<Home/>},
+      {path:'about',element:<About/>},
+      {path:'pricing',element:<Pricing/>},
+      {path:'contact',element:<Contact/>}
+    ]}
+  ])
   return (
     <div className="App">
-      App
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
