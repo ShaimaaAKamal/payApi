f28b6d7212b0b775526cb9fcef3c8a674ccc05c4
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-12 15:02:36 +0200

    implement home  heading component and add it to home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,30 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import phoneHeading from '../../assets/home/desktop/illustration-phone-mockup.svg';
+import HomeHeading from './HomeHeading/HomeHeading';
 
 export default function Home() {
   return (
     <div className='container-md'>
-      <div className="row gy-0">
-         <div className="col-lg-6 imageHomeHeading">
-            <div className='text-center ps-5 ps-md-0'>
-                <img src={phoneHeading} alt="phone Image" className='imageHomeWidth' />
-            </div>
-         </div>
-         <div className="col-lg-6 align-self-center">
-            <div className='text-center text-lg-start'>
-               <h1 className='display-4 fw-semibold mb-4 pb-1 textSanBlue fmHeading'>Start building with our APIs for absolutely free.</h1>
-               <div className="input-group mb-3 position-relative w-90 mx-auto ms-lg-0">
-                <input type="email" id='emailInput'className="form-control shadow form-control-lg rounded-pill" placeholder="Enter Email Address" aria-label="Email" aria-describedby="enterEmailBtn"/>
-                <button className="input-group-text mainBtn px-4 small" id="enterEmailBtn">Schedule a Demo</button>
-              </div>
-              <p className='textLightBlue ps-4'>
-                <span className='small'>Have any Questions</span>
-                <Link className="text-reset text-decoration-none ms-1 fw-bold" to='/contact'>Contact us</Link>
-              </p>
-            </div>
-         </div>
-      </div>
+      <HomeHeading/>
     </div>
   )
 }
