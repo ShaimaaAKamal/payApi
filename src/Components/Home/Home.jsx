6b284d5b86155e67bfdafe8f498d94c48f906fb0
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-12 16:32:45 +0200

    implement who work component and add it to home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HomeHeading from './HomeHeading/HomeHeading';
+import WhoWork from './WhoWork/WhoWork';
 
 export default function Home() {
   return (
-    <div className='container-md'>
+    <>
       <HomeHeading/>
-    </div>
+      <WhoWork/>
+    </>
   )
 }
