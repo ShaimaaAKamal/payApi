2766c5ca488d121c240e1264af2115c73f236eb1
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-13 18:01:10 +0200

    adjust component margin

--- src/Components/ReadyToStart/ReadyToStart.jsx
+++ src/Components/ReadyToStart/ReadyToStart.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ReadyToStart() {
   return (
-    <section className='container-md  py-5 d-flex flex-column flex-lg-row align-items-center justify-content-lg-between '>
+    <section className='container-md  py-5 my-3 d-flex flex-column flex-lg-row align-items-center justify-content-lg-between '>
         <h2 className='display-6 fmHeading textSanBlue mb-4 mb-lg-0'>Ready to start?</h2>
         <div className="input-group position-relative readyToStartWidth">
             <input type="email" id='emailInput'className="form-control shadow form-control-lg rounded-pill" placeholder="Enter Email Address" aria-label="Email" aria-describedby="enterEmailBtn"/>
