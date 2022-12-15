37acf665422ebf3f555f955aa14d79fba3ca4637
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-15 17:31:45 +0200

    adjust not found pages

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import image from '../../assets/download-1.jpg'
 
 export default function NotFound() {
   return (
-    <div>
-      notfound
+    <div className='vh-100 d-flex justify-content-center align-items-center'>
+        <img src={image} alt="not found image" className='w-75' />
     </div>
   )
 }
