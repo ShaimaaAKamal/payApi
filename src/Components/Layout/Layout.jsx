b9e4f498a083dbc807a2d1b8c436c0bc40377526
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-15 16:55:24 +0200

    close nav while navigate

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,12 +1,22 @@
 import React, { useEffect } from 'react';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 export default function Layout() {
+  const location=useLocation();
+  const path=location.pathname;
   useEffect(()=>{
-      
-  },[])
+      const collapse =document.querySelector('.collapse ');   
+      const close=document.querySelector('#close');
+      const menu=document.querySelector('#menu');  
+      if(collapse.classList.contains('d-block')) {
+          collapse.classList.add('d-none');
+          close.classList.add('d-none');
+          menu.classList.remove('d-none');
+          collapse.classList.remove('d-block');
+      }
+  },[path])
   return (
       <div className="bigContainer">
           <Navbar/>
